d2fa70333fb132add862e1dbd3c1f1574eecf3f9
AmirkhanAkhat <230103090@sdu.edu.kz>
2025-05-28 23:34:58 +0500

    REGISTRATION & LOGIN WITH JWT

---
+++ src/main/java/org/SplitLedger/config/JwtUtil.java
@@ -0,0 +1,39 @@
+package org.SplitLedger.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        long expiration = 1000 * 60 * 60 * 10; // 10 hours
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String username) {
+        final String tokenUsername = extractUsername(token);
+        return (tokenUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+}
--- src/main/java/org/SplitLedger/dto/RegisterResponse.java
+++ src/main/java/org/SplitLedger/dto/RegisterResponse.java
@@ -5,11 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterDTO {
+public class RegisterResponse {
+    private Long id;
     private String username;
     private String email;
-    private String password;
+    private LocalDateTime createdAt;
 }
