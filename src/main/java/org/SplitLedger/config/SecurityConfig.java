8abc701712f1abcd87c3ea34103bfe82b647ce84
AmirkhanAkhat <230103090@sdu.edu.kz>
2025-06-22 23:06:24 +0500

    NEW JWT FILTER + OpenApiConfig

--- src/main/java/org/SplitLedger/config/SecurityConfig.java
+++ src/main/java/org/SplitLedger/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package org.SplitLedger.config;
 
+import lombok.RequiredArgsConstructor;
+import org.SplitLedger.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public BCryptPasswordEncoder encoder() {
@@ -17,11 +26,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        return http
                 .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
 }
--- src/main/java/org/SplitLedger/controller/ApiController.java
+++ src/main/java/org/SplitLedger/controller/ApiController.java
@@ -1,27 +1,23 @@
 package org.SplitLedger.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.SplitLedger.config.JwtUtil;
 import org.SplitLedger.dto.DashBoardDTO;
 import org.SplitLedger.service.DashBoardService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
 
-    private final JwtUtil jwtUtil;
     private final DashBoardService dashBoardService;
 
     @GetMapping("/dashboard")
-    public ResponseEntity<DashBoardDTO> getDashBoard(@RequestHeader("Authorization") String authHeader) {
-        String token = authHeader.substring(7);
-        String username = jwtUtil.extractUsername(token);
+    public ResponseEntity<DashBoardDTO> getDashBoard(Authentication authentication) {
+        String username = authentication.getName();
         return ResponseEntity.ok(dashBoardService.getDashboard(username));
     }
 
