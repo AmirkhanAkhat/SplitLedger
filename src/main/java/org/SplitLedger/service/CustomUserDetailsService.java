d56583711b327e457d9b377b12dfd1a9fd1d93e7
AmirkhanAkhat <akhatov2901@gmail.com>
2025-06-22 23:06:24 +0500

    NEW JWT FILTER + OpenApiConfig

---
+++ src/main/java/org/SplitLedger/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.SplitLedger.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.SplitLedger.entity.User;
+import org.SplitLedger.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
